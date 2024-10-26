084ab9ed956b2ed0201ce95561d6d5855d5503ce
Brian Bird <ProfBird@users.noreply.github.com>
2024-10-26 13:00:14 -0700

    Added controller methods for managing review submission.

--- BookReviews2024/Controllers/ReviewsController.cs
+++ BookReviews2024/Controllers/ReviewsController.cs
@@ -8,15 +8,23 @@ namespace BookReviews2024.Controllers
         public IActionResult Index()
         {
             // TODO: Get the review objects and put them into a list.
-            
-            
-            return View();
+            Review model = new Review();
+            model.Reviewer = new AppUser();
+            model.Book = new Book();
+            return View(model);
         }
 
         public IActionResult Review()
         {
 
-        return View(); 
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Review(Review model)
+        {
+            model.ReviewDate = DateTime.Now;  // Add date and time to the model
+            return View("Index", model);
         }
     }
 }
